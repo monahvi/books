be94389a04f7a498ea7b9f50db3f431aa81d6d0d
monahvi <nahvi.moghaddam@gmail.com>
2023-08-13 12:49:11 +0100

    create a book

--- src/App.js
+++ src/App.js
@@ -1,5 +1,18 @@
+import { useState } from "react";
+import BookCreate from "./components/BookCreate";
+
 function App () {
-  return<div>App</div>;
+  const [books, setBooks] = useState([]);
+
+  const creatBook = (title) => {
+    console.log('Need to add a book with:', title);
+  };
+
+  return(
+  <div>
+    < BookCreate onCreate={creatBook}/>
+  </div>
+  );
 }
 
 export default App;
