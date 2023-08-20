d505e0c1270d24550f05dd78b56e8ba86357fba5
monahvi <nahvi.moghaddam@gmail.com>
2023-08-20 22:00:18 +0100

    useContect

--- src/components/BookCreate.js
+++ src/components/BookCreate.js
@@ -1,28 +1,30 @@
-import { useState } from "react";
+import { useState, useContext } from 'react';
+import BooksContext from '../context/books';
 
+function BookCreate() {
+  const [title, setTitle] = useState('');
+  const { createBook } = useContext(BooksContext);
 
-function BookCreate ({ onCreate }) {
-    const [title, setTitle] = useState('');
+  const handleChange = (event) => {
+    setTitle(event.target.value);
+  };
 
-    const handleChange = (event) => {
-        setTitle(event.target.value);
-    };
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    createBook(title);
+    setTitle('');
+  };
 
-    const handleSubmit = (event) => {
-        event.preventDefault();
-        onCreate(title);
-        setTitle('');
-    };
-
-    return (
+  return (
     <div className="book-create">
-        <h3>Add a Book</h3>
-        <form onSubmit={handleSubmit}>
-            <label>Title</label>
-            <input className="input" value={title} onChange={handleChange}/>
-            <button className="button">Create!</button>
-        </form>
-    </div>);
+      <h3>Add a Book</h3>
+      <form onSubmit={handleSubmit}>
+        <label>Title</label>
+        <input className="input" value={title} onChange={handleChange} />
+        <button className="button">Create!</button>
+      </form>
+    </div>
+  );
 }
 
 export default BookCreate;
